57071e8419b8ee53e8155a387a66f182064a34f6
haha4825 <tw086219@gmail.com>
2025-07-24 10:15:04 +0900

    init project

---
+++ heeseo/src/test/java/mutsa/heeseo/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mutsa.heeseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
